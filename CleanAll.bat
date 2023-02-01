2bd2e76020ea7daf4ae8624c47f80a6ba2ee6c4b
David-MolTor <david.molinatoro@outlook.com>
2023-02-01 11:37:21 +0100

    Configuration and icons
    Added new application icons.
    Added new ignore conditions.

--- CleanAll.bat
+++ CleanAll.bat
@@ -1,5 +1,2 @@
-RMDIR "./MidiControl/bin/Debug" /S /Q
-RMDIR "./MidiControl/bin/Release" /S /Q
-
-RMDIR "./MidiControl/obj/Debug" /S /Q
-RMDIR "./MidiControl/obj/Release" /S /Q
+RMDIR "./MidiControl/bin" /S /Q
+RMDIR "./MidiControl/obj" /S /Q
