cd4364212c67dd9f506b9522c1e035a2f0bbfd86
David-MolTor <david.molinatoro@outlook.com>
2023-02-01 12:29:08 +0100

    MIDI library and initial connection
    Added an external MIDI library.
    Added the initial connection system.

---
+++ MidiControl/Constants.cs
@@ -0,0 +1,22 @@
+﻿/*
+MIDI CONTROL 2023
+
+Constants.cs
+
+- Description: Constants declaration script
+- Author: David Molina Toro
+- Date: 01 - 02 - 2023
+- Version: 0.1
+*/
+
+namespace MidiControl
+{
+    internal class Constants
+    {
+        //Connection checking period in milliseconds
+        public const int CONNECTION_PERIOD = 2000;
+
+        //DL4 MIDI device name
+        public const string DL4_PRODUCT_NAME = "DL4 MkII MIDI";
+    }
+}
