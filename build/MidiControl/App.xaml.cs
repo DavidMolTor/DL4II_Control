91c1af5d61923d4e3efa4b4d70e3ec015166f3c0
David-MolTor <david.molinatoro@outlook.com>
2023-12-15 09:05:10 +0100

    Main window WPF
    Moved the main window form to WPF.
    Updated resources and configurations.

---
+++ build/MidiControl/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿/*
+MIDI CONTROL 2023
+
+App.cs
+
+- Description: Contains the application entry point
+- Author: David Molina Toro
+- Date: 15 - 12 - 2023
+- Version: 0.1
+*/
+
+using System.Windows;
+
+namespace MidiControl
+{
+    public partial class App : Application
+    {
+
+    }
+}
---
+++ build/MidiControl/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿/*
+MIDI CONTROL 2023
+
+MainWindow.cs
+
+- Description: Main window control
+- Author: David Molina Toro
+- Date: 15 - 12 - 2023
+- Version: 0.1
+*/
+
+using System.Windows;
+
+namespace MidiControl
+{
+    public partial class MainWindow : Window
+    {
+        /*
+        Public constructor
+        */
+        public MainWindow()
+        {
+            //Load the main configuration
+            IControlConfig.Instance.LoadConfig();
+
+            //Set the MIDI control system
+            IControlMIDI.Instance.Initialize();
+
+            InitializeComponent();
+        }
+
+        /*
+        Window closing event handler
+        */
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            //Reset the MIDI control system
+            IControlMIDI.Instance.Deinitialize();
+        }
+    }
+}
